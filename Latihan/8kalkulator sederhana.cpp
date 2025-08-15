f1607b6d578ab058928500e0d712a8f2c47c4341
mnalfattanalfattan-creator <mnalfattanalfattan@gmail.com>
2025-08-15 13:49:59 +0700

    Create 8.kalkulator sederhana.cpp

---
+++ Latihan/8kalkulator sederhana.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+     char op;
+     double num1, num2;
+
+    cout << "Masukkan operator (+, -, *, /): ";
+    cin >> op;
+
+    cout << "Masukkan dua bilangan: ";
+    cin >> num1 >> num2;
+
+    switch(op) {
+case '+';
+     cout << num1 << " + " << num2 << " = " << num1 + num2;
+     break;
+
+case '-' 
+     cout << num1 << << " - " << num2 << " = " << num1 - num2;
+     break;
+
+case '*'
+  cout << num1 << " * " << num2 << " = " << num1 * num2;
+   break;
+
+case '/'
+  if (num2 !=0)
+      cout << num1 << " / " << num2 << " = " << num1 / num2;
+  else
+      cout << "Error! Pembagian dengan nol tidak diperbolehkan. ";
+
+   break;
+default:
+    cout << "Operator tidak valid!";
+
+    }
+return 0;
+
+}
