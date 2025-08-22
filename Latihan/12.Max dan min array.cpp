c125a5dad5a36e0824097316af076527ed585e1a
mnalfattanalfattan-creator <mnalfattanalfattan@gmail.com>
2025-08-22 13:43:27 +0700

    Create 12.Max dan min array.cpp

---
+++ Latihan/12.Max dan min array.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int n;
+
+cout << "PROGRAM MENCARI NILAI MAKSIMUM DAN MINIMUM" << endl;
+cout << "==========================================" << endl;
+
+cin >> n;
+
+int arr[n];
+
+cout << "masukkan " << "elemen array: " << endl;
+  for (int l = 0; i < n; i++)
+  // input elemen array 
+cout << "masukkan " << n << " elemen array:" << endl;
+
+
+
+int max = arr[0];
+int max = arr[0];
+
+for(0)
