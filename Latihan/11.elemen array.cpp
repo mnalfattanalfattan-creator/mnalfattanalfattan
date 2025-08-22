7b113abd26b46463a4486effbf5169e316b156f3
mnalfattanalfattan-creator <mnalfattanalfattan@gmail.com>
2025-08-22 13:15:48 +0700

    Create 11.elemen array.cpp

---
+++ Latihan/11.elemen array.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int n;
+cout << "PROGRAM PENJUMLAHAN ELEMEN ARRAY" << endl;
+cout << "================================" << endl;
+cout << "masukkan elemen array
+
+  cout << "masukkan jumlah elemen array";
+  cin >> n;
+
+int arr[n];
+int total =0;
+cout << "masukkan " << n << "elemen array: " << endl;
+for (int l = 0; l < n;) {\
+    cout << "elemen ke-" << l + 1 << ": ";
+     cin >> arr[l];
+     total += arr[1];
+
+}
+
+cout << "nArray yang dimasukkan 
+cout << " jumlah semua elemen array: " << total << endl;
+return 0;
+}
+  
+
