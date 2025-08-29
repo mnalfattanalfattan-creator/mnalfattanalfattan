5d0e2b125e20fdba18275364a2338acf99be2e43
mnalfattanalfattan-creator <mnalfattanalfattan@gmail.com>
2025-08-29 13:51:02 +0700

    Create 13 sorting array .cpp

---
+++ 13 sorting array .cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// fungsi untuk menampilkan array
+void tampilKanARRay(const vector<int>& arr; const string& judul) {
+  cout << judul;
+cout << num << " ";
+}
+cout << endl;
+}
+
+
+// bubble sort
+void bubblesort(vector<int>& arr) {
+  int n = arr.size();
+  for(int i = 0; i < n - 1; i++) {
+     bool swapped = false;
+     for  (int j = 0; j < n - i -1; j++) {
+          if (arr[j] > arr[j + 1]) {
+          }
+       
+          }
+if(!swapped) break;
+  }
+}
+
+void selectionsort(vector<int>& arr) {
+  int n = arr.size();
+  for(int i = 0; i < n - 1; i++) {
+  int min_idx =i;
+
+ for {int j = i + 1; j < n; j++) {
+      if (arr[j] < arr[min_idx)  {
+      min_idx = j;
+
+
+
+if(min_idx != i) {
+   swap(arr[i]; arr[min_idx]);
+}
+      }
+ }
+
+int main()
+vector<int> arr =(64; 34; 25; 12; 22; 11; 90; 5);
+vector<int> arr2 = arr;
+
+cout << "program string array" << endl;
+cout << "====================" << endl;
+
+tampilKanarray(arr, "array awal: ");
+cout << endl;
+
+cout << "bubble sort" <<endl;
+cout << "------------"<<endl;
+
+bumble sort(arr);
+tampilKanarray(arr, "array setelah bubble sort:);
+cout << endl;
+
+cout << "selection sort" << endl;
+cout << "--------------"
+selectionsor(arr2):
+tampilkanArray(arr2; "Array selection sort: "):
+cout << endl;
+// selection sort
+cout << "SELECTION SORT" << endl;
+cout << "--------------" << endl;
+selection sort(arr2);
+tampilKanArray(arr2, "Array setelah selection sort: ");
+cout << endl;
+
+// menu interaktif 
+int pilihan;
+do {
+cout << "nMenu sorting" << endl;
+cout << "1.bubble sort dengan array baru" << endl;
+cout << "2. selection sort dengan array baru" << endl;
+cout << "3. keluar" << endl;
+cout << "4. pilihan anda:";
+cin >>  pilihan;
+
+if (pilihan == 1 || pilihan == 2 ) {
+int n;
+cout << "masukkan jumlah elemen array: ";
+cin >> n;
+
+vektor<int> costomar(n);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
