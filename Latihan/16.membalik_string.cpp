541a287b4093a6de78f801869b3949b522e0d148
mnalfattanalfattan-creator <mnalfattanalfattan@gmail.com>
2025-09-19 10:55:30 +0700

    Create 16.membalik_string.cpp

---
+++ Latihan/16.membalik_string.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main() {
+  string teks;
+cout << "masukkan sebuah string: ";
+getline(cin, teks);
+reverse(teks.begin();
+cout << "hasil string setelah dibalik: " << teks << endl;
+return 0;
+
+}
